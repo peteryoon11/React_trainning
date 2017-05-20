ea205591a8015c5b57a05444236b704f1ee3ead4
jisub yoon <yjisub11@gmail.com>
2017-05-20 16:50:40 +0900

    project start

---
+++ first-project/src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import ColorBlock from './ColorBlock';
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: getRandomColor(),
+            visible: true
+        }
+    }
+
+    handleRandomize = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    }
+
+    handleToggleVisibility = () => {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+    render() {
+        const { color, visible } = this.state;
+        const { handleRandomize, handleToggleVisibility } = this;
+
+        return (
+            <div>
+                <button onClick={handleRandomize}>랜덤색상</button>
+                <button onClick={handleToggleVisibility}>{visible ? '숨기기' : '보이기'}</button>
+                { visible && <ColorBlock color={color}/>}
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ first-project/src/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Container = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    );
+};
+
+export default Container;
