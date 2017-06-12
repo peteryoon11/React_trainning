05a6cff7f8393b1f3f91bc9f1fbfc186beacf200
jisub yoon <yjisub11@gmail.com>
2017-06-12 11:14:14 +0900

    reupload

--- redux-trainning-a/src/index.js
+++ redux-trainning-a/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<App />, document.getElementById('root'));
